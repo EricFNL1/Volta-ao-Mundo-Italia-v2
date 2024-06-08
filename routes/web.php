0c98c4ce5d263c4db4d7460a886c7d42d5c77b7c
Eric Junior <eric.faria2003@gmail.com>
2024-06-08 13:40:22 -0300

    Corrigido Bugs de conexão com o Banco de dados e Controlador de paginas

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaginaController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('index');
 });
 
 Route::get('/', [PaginaController::class, 'index'])->name('index');
@@ -17,3 +14,14 @@ Route::get('/pontturisticos', [PaginaController::class, 'pontturisticos'])->name
 Route::get('/historia', [PaginaController::class, 'historia'])->name('historia');
 Route::get('/formulario', [PaginaController::class, 'formulario'])->name('formulario');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
